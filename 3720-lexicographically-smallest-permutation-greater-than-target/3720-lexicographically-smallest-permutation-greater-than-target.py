90ff1325b5143665cb8aba1312842ad79e3e668a
Aryan Rath <143625019+aryanrath123@users.noreply.github.com>
2026-08-27 10:34:52 +0530

    Time: 6 ms (92.11%), Space: 19.3 MB (71.05%) - LeetHub

--- 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
+++ 3720-lexicographically-smallest-permutation-greater-than-target/3720-lexicographically-smallest-permutation-greater-than-target.py
@@ -1,32 +1,36 @@
+INVALID='{'
 class Solution:
     def lexGreaterPermutation(self, s: str, target: str) -> str:
-        cnt = [0] * 26
-
-        for ch in s:
-            cnt[ord(ch) - ord('a')] += 1
-
-        ans = ""
-
-        for i in range(len(target)):
-            x = ord(target[i]) - ord('a')
-
-            # Try making this position larger
-            for c in range(x + 1, 26):
-                if cnt[c]:
-                    res = target[:i] + chr(c + ord('a'))
-                    cnt[c] -= 1
-
-                    for j in range(26):
-                        res += chr(j + ord('a')) * cnt[j]
-
-                    ans = res
-                    cnt[c] += 1
-                    break
-
-            # Use target[i] if possible and continue
-            if cnt[x] == 0:
+        c=Counter(s)
+        n=len(s)
+        endi = n
+        for i in range(n):
+            ch = target[i]
+            if ch not in c:
+                endi = i
                 break
+            else:
+                c[ch]-=1
+                if c[ch]==0: c.pop(ch)
+        if endi==n:
+            endi=n-1
+            c[target[endi]]+=1
+        # print(c, endi)
+        for i in range(endi, -1, -1):
+            ch = target[i]
+            x=INVALID
+            for y in c.keys():
+                if y>ch and y<x: x=y
+            if x!=INVALID:
+                res=[target[:i], x]
+                c[x]-=1
+                for y in sorted(c.keys()):
+                    res.append(y*c[y])
+                return ''.join(res)
+            elif i>0:
+                c[target[i-1]]+=1
+        return "" 
+        # print(endi, c)
+        # return s
 
-            cnt[x] -= 1
-
-        return ans
+        
